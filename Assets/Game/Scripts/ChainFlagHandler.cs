af7ed7028d4bad9cbe7ca045df61a69e841c2e17
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2025-08-05 14:54:21 +0100

    add CHAIN_SharedData helper component

---
+++ Assets/Game/Scripts/ChainFlagHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    /// <summary>
+    /// Allows to Create, Delete or Check for a given flag in CHAIN_SharedData
+    /// </summary>
+    public class ChainFlagHandler : MonoBehaviour
+    {
+        [SerializeField] private string targetFlag;
+        
+        public void CreateFlag()
+        {
+            CHAIN_SharedData.CreateFlag(targetFlag);
+        }
+
+        public void DeleteFlag()
+        {
+            CHAIN_SharedData.DeleteFlag(targetFlag);
+        }
+
+        public bool DoesFlagExist()
+        {
+            return CHAIN_SharedData.DoesFlagExist(targetFlag);
+        }
+    }
+}
