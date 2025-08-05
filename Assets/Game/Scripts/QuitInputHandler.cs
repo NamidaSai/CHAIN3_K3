70a33cd965880ff1f784a69e30388ff51f0b0b91
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2025-08-05 09:59:29 +0100

    refactor quit input

---
+++ Assets/Game/Scripts/QuitInputHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game
+{
+    public class QuitInputHandler : MonoBehaviour
+    {
+        private InputAction _quitAction;
+        
+        private void Awake()
+        {
+            _quitAction = InputSystem.actions.FindAction("Quit");
+            _quitAction.performed += HandleQuit;
+        }
+
+        private void OnDestroy()
+        {
+            _quitAction.performed -= HandleQuit;
+        }
+
+        private void HandleQuit(InputAction.CallbackContext context)
+        {
+            Debug.Log("Quit");
+            QuitGame();
+        }
+        
+        private void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
