46a44d774d9beaa14f2776925d21dcf10a23e552
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2025-08-06 13:26:52 +0100

    feat: allow repeatable dialogue

--- Assets/Game/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Game/Scripts/Dialogue/DialogueTrigger.cs
@@ -5,6 +5,7 @@ namespace Game.Dialogue
     public class DialogueTrigger : MonoBehaviour
     {
         [SerializeField] private DialoguePart dialogueToPlay;
+        [SerializeField] private bool isRepeatable = false;
 
         private bool _wasTriggered = false;
 
@@ -17,6 +18,13 @@ namespace Game.Dialogue
             
             _wasTriggered = true;
             DialogueSystem.Instance.StartDialogue(dialogueToPlay);
+            DialogueSystem.Instance.onDialogueEnd.AddListener(HandleDialogueEnd);
+        }
+
+        private void HandleDialogueEnd()
+        {
+            _wasTriggered = !isRepeatable;
+            DialogueSystem.Instance.onDialogueEnd.RemoveListener(HandleDialogueEnd);
         }
     }
 }
