1521ecbbd6c7ad8301bab41aff603a48a3b6c2e2
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2025-08-07 16:47:20 +0100

    feat: add skip typewriter

---
+++ Assets/Game/Scripts/SkipTypewriter.cs
@@ -0,0 +1,30 @@
+﻿using Febucci.UI;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game
+{
+    [RequireComponent(typeof(TypewriterByCharacter))]
+    public class SkipTypewriter : MonoBehaviour
+    {
+        private InputAction _interactAction;
+        private TypewriterByCharacter _typewriter;
+        
+        private void Awake()
+        {
+            _interactAction = InputSystem.actions.FindAction("Interact");
+            _interactAction.performed += HandleInteract;
+            _typewriter = GetComponent<TypewriterByCharacter>();
+        }
+
+        private void OnDestroy()
+        {
+            _interactAction.performed -= HandleInteract;
+        }
+
+        private void HandleInteract(InputAction.CallbackContext context)
+        {
+            _typewriter.SkipTypewriter();
+        }
+    }
+}
